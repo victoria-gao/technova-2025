bf240955b71730fec96ad377606e4daeb17a2471
xena-p <xenap26@gmail.com>
2025-09-27 16:12:03 -0400

    in progress signup

--- backend/user/models.py
+++ backend/user/models.py
@@ -7,18 +7,41 @@ from db import collection
 from bson.objectid import ObjectId
 mongo = PyMongo()
 class User:
-    def signup(username, email):
-        """Insert a new user into MongoDB"""
-        user = {"username": username, "email": email}
-        result = collection.insert_one(user)
-        return str(result.inserted_id)
+        def google_signup(google_user_info):
+    
+    # google_user_info = {
+    #     "google_id": str,
+    #     "email": str,
+    #     "name": str
+    # }
+            existing = collection.find_one({"google_id": google_user_info["google_id"]})
+            if existing:
+                return str(existing["_id"])  # user already exists
 
-    def get_all_users():
-        """Return all users as a list of dicts"""
-        users = list(collection.find())
-        for u in users:
-            u["_id"] = str(u["_id"])
-        return users
+            result = collection.insert_one(google_user_info)
+            return str(result.inserted_id)
+            
+        def get_user_by_google_id(google_id):
+            return collection.find_one({"google_id": google_id})
+        # def __init__(self, username):
+        #     self.username = username
+
+        # def __repr__(self):
+        #     return f"<User {self.username}>"
+
+
+    # def signup(username, email):
+    #     """Insert a new user into MongoDB"""
+    #     user = {"username": username, "email": email}
+    #     result = collection.insert_one(user)
+    #     return str(result.inserted_id)
+
+    # def get_all_users():
+    #     """Return all users as a list of dicts"""
+    #     users = list(collection.find())
+    #     for u in users:
+    #         u["_id"] = str(u["_id"])
+    #     return users
 
 # class User:
 #     def google_signup(google_user_info):
--- backend/user/routes.py
+++ backend/user/routes.py
@@ -1,16 +1,15 @@
 from flask import Blueprint, request, jsonify
-# from .models import google_signup
-# from google.oauth2 import id_token
-# from google.auth.transport import requests as grequests
-# import os
-# from dotenv import load_dotenv
+from google.oauth2 import id_token
+from google.auth.transport import requests as grequests
+import os
+from dotenv import load_dotenv
 
 from user.models import User
 #from . import user_bp
 #from user.models import User
 
-# load_dotenv()
-# GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+load_dotenv()
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 
 user_bp = Blueprint('user', __name__)
 
@@ -18,18 +17,35 @@ user_bp = Blueprint('user', __name__)
 
 
 
-@user_bp.route("/signup", methods=["POST"])
-def signup():
+    # @user_bp.route("/signup", methods=["POST"])
+    # def signup():
+    #     data = request.json
+    #     username = data.get("username")
+    #     email = data.get("email")
+    #     if not username or not email:
+    #         return jsonify({"error": "Missing username or email"}), 400
+    #     user_id = User.signup(username, email)  # call function from models.py
+    #     return jsonify({"message": "User created", "id": user_id})
+
+    # @user_bp.route("/users", methods=["GET"])
+    # def users():
+    #     users = User.get_all_users()  # call function from models.py
+    #     return jsonify(users)
+
+@user_bp.route("/google-signup", methods=["POST"])
+def google_signup_route():
+    """
+    Frontend should POST google user info here:
+    {
+        "google_id": "...",
+        "email": "...",
+        "name": "..."
+    }
+    """
     data = request.json
-    username = data.get("username")
-    email = data.get("email")
-    if not username or not email:
-        return jsonify({"error": "Missing username or email"}), 400
-    user_id = User.signup(username, email)  # call function from models.py
-    return jsonify({"message": "User created", "id": user_id})
+    required_fields = ["google_id", "email", "name"]
+    if not all(field in data for field in required_fields):
+        return jsonify({"error": "Missing Google user info"}), 400
 
-@user_bp.route("/users", methods=["GET"])
-def users():
-    users = User.get_all_users()  # call function from models.py
-    return jsonify(users)
-
+    user_id = User.google_signup(data)
+    return jsonify({"message": "User signed up with Google", "id": user_id})
