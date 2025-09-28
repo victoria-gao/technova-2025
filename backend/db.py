e8319181533f70cafe8fff2987598311dbe33340
Sandy Banh <sandybanh2525@gmail.com>
2025-09-27 22:59:49 -0400

    initial

--- backend/db.py
+++ backend/db.py
@@ -1,18 +1,20 @@
-import pymongo 
-import os
-
+# db.py
+import os, certifi, pymongo
 from dotenv import load_dotenv
 
 load_dotenv()
-
-MONGO_PASS = os.environ.get("MONGO_PASS")
-
+MONGO_PASS = os.getenv("MONGO_PASS")
 if not MONGO_PASS:
-    raise ValueError("MONGOPASS environment variable is not set.")
+    raise ValueError("MONGO_PASS environment variable is not set.")
 
-#Xenas uri
-uri = f"mongodb+srv://xenap26_db_user:{MONGO_PASS}@cluster0.htg8qve.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+URI = f"mongodb+srv://sandy_db_user:{MONGO_PASS}@cluster0.htg8qve.mongodb.net/?retryWrites=true&appName=Cluster0"
 
-cluster = pymongo.MongoClient(uri)
-db = cluster["UserData"]
-collection = db["flask_mongo"]
+client = pymongo.MongoClient(
+    URI,
+    tls=True,
+    tlsCAFile=certifi.where(),
+    serverSelectionTimeoutMS=30000,
+)
+db = client["UserData"]
+users_col = db["flask_mongo"]
+items_col = db["items"]
